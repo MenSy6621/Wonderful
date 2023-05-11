e3ec5c3e154a74bc0432025696d02caa83a55ab6
MenSy6621 <130986974+MenSy6621@users.noreply.github.com>
2023-05-11 17:07:25 +0600

    Add files via upload

---
+++ js/Slider.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    autoHeight: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+},
+    slidesPerGroup: 1,
+    grabCursor: true,
+    });
