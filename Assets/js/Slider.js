971e2f0d72eaa321be1f6b0a6918733c44f1d76e
MenSy6621 <130986974+MenSy6621@users.noreply.github.com>
2023-05-13 00:43:27 +0600

    Add files via upload

--- Assets/js/Slider.js
+++ Assets/js/Slider.js
@@ -3,9 +3,32 @@ var swiper = new Swiper('.swiper', {
     loop: true,
     autoHeight: true,
     autoplay: {
-        delay: 5000,
+        delay: 8000,
         disableOnInteraction: false
 },
     slidesPerGroup: 1,
     grabCursor: true,
     });
+    const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+    const appendAlert = (message, type) => {
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+        ].join('')
+    
+        alertPlaceholder.append(wrapper)
+    }
+    const alertTrigger = document.getElementById('liveAlertBtn')
+    if (alertTrigger) {
+        alertTrigger.addEventListener('click', () => {
+        appendAlert('Nice, you triggered this alert message!', 'success')
+        })
+    }
+    const form = document.querySelector('form')
+    const btn = document.querySelector('#liveAlertBtn')
+    btn.addEventListener('click', () => {
+        form.reset()
+    })
